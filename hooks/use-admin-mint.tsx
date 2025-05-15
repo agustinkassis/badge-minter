eba953f68eacf3da5db61ce499dc84aa92f6d9fe
Agustin Kassis <webmaster@masize.com>
2025-05-15 17:54:56 -0300

    add respond claim

--- hooks/use-admin-mint.tsx
+++ hooks/use-admin-mint.tsx
@@ -5,6 +5,10 @@ import { useNostrAdmin } from '@/contexts/nostr-admin-context'
 import { NostrEvent } from '@nostrify/nostrify'
 import { BadgeAward } from '@/types/badge'
 import { useBadges } from '@/hooks/use-badges'
+import { EventTemplate } from 'nostr-tools'
+import { useNostr } from '@nostrify/react'
+import { getTagValue } from '@/lib/nostr'
+import { ClaimResponseKind } from '@/types/claim'
 
 export interface UseAdminMintProps {
   onNewAward?: (award: BadgeAward) => void
@@ -22,8 +26,36 @@ export const useAdminMint = ({
   const [nonceService] = useState(() => new NonceService())
   const { currentBadge } = useNostrAdmin()
   const { awards, award } = useBadges({ badge: currentBadge })
-
+  const { signer } = useNostrAdmin()
   const now = useMemo(() => Date.now() / 1000, [])
+  const { nostr } = useNostr()
+
+  const respondClaim = useCallback(
+    async (award: BadgeAward) => {
+      console.info('Response claim received', award)
+
+      const claimEvent = award.event
+
+      const event = {
+        content: JSON.stringify(award),
+        kind: ClaimResponseKind,
+        tags: [
+          ['p', claimEvent.pubkey],
+          ['a', getTagValue(claimEvent, 'a')],
+          ['e', claimEvent.id]
+        ],
+        created_at: Math.floor(Date.now() / 1000)
+      } as EventTemplate
+
+      const signedEvent = await signer?.signEvent(event)!
+
+      console.info('*** Sending response claim ***')
+      console.dir(signedEvent)
+
+      nostr.event(signedEvent)
+    },
+    [signer, nostr]
+  )
 
   const onClaimRequest = useCallback(
     async (event: NostrEvent) => {
@@ -33,9 +65,11 @@ export const useAdminMint = ({
         currentBadge!,
         JSON.parse(event.content)
       )
+
+      respondClaim(badgeAward)
       onNewAward?.(badgeAward)
     },
-    [currentBadge, award, onNewAward]
+    [currentBadge, award, onNewAward, respondClaim]
   )
 
   // Starts claimRequests subscription
