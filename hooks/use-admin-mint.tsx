d392b10de16ed741be722e988f6ffa7c9608aa53
Agustin Kassis <webmaster@masize.com>
2025-05-15 14:19:30 -0300

    implement claim and award draft flow

--- hooks/use-admin-mint.tsx
+++ hooks/use-admin-mint.tsx
@@ -1,16 +1,50 @@
-import { useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { NonceService } from '@/lib/nonce-service'
+import { useClaimRequests } from './use-claim-requests'
+import { useNostrAdmin } from '@/contexts/nostr-admin-context'
+import { NostrEvent } from '@nostrify/nostrify'
+import { BadgeAward } from '@/types/badge'
+import { useBadges } from '@/hooks/use-badges'
 
-export interface UseAdminMintProps {}
+export interface UseAdminMintProps {
+  onNewAward?: (award: BadgeAward) => void
+}
 
 export interface UseAdminMintReturn {
   generateNonce: () => string
   burnNonce: (nonce: string) => boolean
+  awards: BadgeAward[]
 }
 
-export const useAdminMint = (): UseAdminMintReturn => {
-  //   const { currentBadge } = useNostrAdmin()
+export const useAdminMint = ({
+  onNewAward
+}: UseAdminMintProps = {}): UseAdminMintReturn => {
   const [nonceService] = useState(() => new NonceService())
+  const { currentBadge } = useNostrAdmin()
+  const { awards, award } = useBadges({ badge: currentBadge })
+
+  const now = useMemo(() => Date.now() / 1000, [])
+
+  const onClaimRequest = useCallback(
+    async (event: NostrEvent) => {
+      console.info('Claim request received', event)
+      const badgeAward = await award(
+        event.pubkey,
+        currentBadge!,
+        JSON.parse(event.content)
+      )
+      onNewAward?.(badgeAward)
+    },
+    [currentBadge, award, onNewAward]
+  )
+
+  // Starts claimRequests subscription
+  useClaimRequests({
+    since: now,
+    until: Infinity,
+    badge: currentBadge || undefined,
+    onClaimRequest
+  })
 
   const generateNonce = () => {
     return nonceService.generateNonce()
@@ -20,5 +54,5 @@ export const useAdminMint = (): UseAdminMintReturn => {
     return nonceService.verifyNonce(nonce)
   }
 
-  return { generateNonce, burnNonce }
+  return { generateNonce, burnNonce, awards }
 }
