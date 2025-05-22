740af5a041083e4206909d7548893a2955e78c7e
Agustin Kassis <webmaster@masize.com>
2025-05-21 22:40:17 -0300

    fixed badge definition subscription

--- hooks/use-badges-definitions.tsx
+++ hooks/use-badges-definitions.tsx
@@ -3,8 +3,9 @@
 import { BadgeDefinition } from '@/types/badge'
 import { BadgeDefinition as BadgeDefinitionKind } from 'nostr-tools/kinds'
 import { naddrEncode } from 'nostr-tools/nip19'
-import { useEffect, useState } from 'react'
-import { useNostr } from '@nostrify/react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useSubscription } from './use-subscription'
+import { NostrEvent, NostrFilter } from '@nostrify/nostrify'
 
 interface UseBadgesDefinitionsProps {
   pubkey: string
@@ -26,37 +27,42 @@ export const useBadgesDefinitions = ({
 }: UseBadgesDefinitionsProps): UseBadgesDefinitionsReturn => {
   const [badges, setBadges] = useState<BadgeDefinition[]>([])
   const [isLoading, setIsLoading] = useState(false)
-  const { nostr } = useNostr()
+
+  const filters: NostrFilter = useMemo(
+    () => ({ authors: [pubkey], kinds: [BadgeDefinitionKind] }),
+    [pubkey]
+  )
+  const handleEvent = useCallback((event: NostrEvent) => {
+    console.log('event', event)
+  }, [])
+
+  const { events } = useSubscription({
+    filter: filters,
+    onEvent: handleEvent
+  })
 
   useEffect(() => {
-    if (!pubkey || !nostr) return
-    setIsLoading(true)
-    nostr
-      .query([{ authors: [pubkey], kinds: [BadgeDefinitionKind] }])
-      .then(events => {
-        setBadges(
-          events.map(event => {
-            const identifier = getTagValue(event, 'd')!
-            const naddr = naddrEncode({
-              identifier,
-              pubkey: event.pubkey,
-              kind: BadgeDefinitionKind
-            })
-            return {
-              id: identifier,
-              name: getTagValue(event, 'name') || 'No name',
-              description:
-                getTagValue(event, 'description') || 'No description',
-              image: getTagValue(event, 'image') || 'No image',
-              pubkey: event.pubkey,
-              naddr: naddr
-            } as BadgeDefinition
-          })
-        )
-        setIsLoading(false)
+    if (!events) return
+
+    setBadges(
+      events.map(event => {
+        const identifier = getTagValue(event, 'd')!
+        const naddr = naddrEncode({
+          identifier,
+          pubkey: event.pubkey,
+          kind: BadgeDefinitionKind
+        })
+        return {
+          id: identifier,
+          name: getTagValue(event, 'name') || 'No name',
+          description: getTagValue(event, 'description') || 'No description',
+          image: getTagValue(event, 'image') || 'No image',
+          pubkey: event.pubkey,
+          naddr: naddr
+        } as BadgeDefinition
       })
-      .catch(() => setIsLoading(false))
-  }, [pubkey, nostr])
+    )
+  }, [events, filters])
 
   return { badges, isLoading }
 }
