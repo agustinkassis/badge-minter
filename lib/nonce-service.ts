53ee32a1c6e0251eeb9705fba35ea0f00f6863fe
Agustin Kassis <webmaster@masize.com>
2025-05-26 20:15:23 -0700

    remove comments from nonce

--- lib/nonce-service.ts
+++ lib/nonce-service.ts
@@ -10,30 +10,15 @@ export class NonceService {
   }
 
   generateNonce(naddr: string = '', time: number): string {
-    // Generate a random string for the nonce
-
     const nonceValue = Buffer.from(
       sha256(Buffer.from(`${time}${naddr}${this.randomSalt}`))
     ).toString('hex')
-
-    console.info('Generating nonce for', naddr, 'and time', time)
-    console.info('Random salt', this.randomSalt)
-    console.info('naddr', naddr)
-    console.info('time', time)
-    console.info('nonce', nonceValue)
 
     return nonceValue
   }
 
   verifyNonce(nonce: string, naddr: string, time: number): boolean {
     const correctNonce = this.generateNonce(naddr, time)
-    console.info('randomSalt', this.randomSalt)
-    console.info('naddr', naddr)
-    console.info('time', time)
-
-    console.info('Nonce to verify', nonce)
-    console.info('Correct nonce', correctNonce)
-
     return nonce === correctNonce
   }
 }
